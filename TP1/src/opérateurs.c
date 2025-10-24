4cde5582f89f46f3098f7c318b1429605488110d
Shoko <a.breyton.pro@gmail.com>
2025-10-24 10:31:58 +0200

    Modif opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -8,7 +8,12 @@ int main() {
     printf("a - b = %d\n", a - b);
     printf("a * b = %d\n", a * b);
 
-    printf("a / b = %s\n", b != 0 ? "16 / 3 = 5" : "Erreur division par 0");
+    // Division avec vérification
+    if (b != 0) {
+        printf("a / b = %d\n", a / b);
+    } else {
+        printf("Erreur : division par 0\n");
+    }
 
     printf("a %% b = %d\n", a % b);
     printf("a == b : %d\n", a == b);
